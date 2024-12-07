45832b9f78fcc62f9972fec86df602ece94c4188
{UserTamura} <177226849+UserTamura@users.noreply.github.com>
2024-12-07 13:51:30 +0900

    Install ESlint

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
+
+  let test: number = 1
+  test = "test"
+
+  const testFunc = ():string => {
+    return "test"
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!!!!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +23,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
-  },
-});
+    justifyContent: "center"
+  }
+})
