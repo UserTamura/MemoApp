472a0315d4e99ed44cdb9dbdd6642aa48ffce587
{UserTamura} <177226849+UserTamura@users.noreply.github.com>
2024-12-07 13:52:53 +0900

    保存忘れ

--- App.tsx
+++ App.tsx
@@ -1,15 +1,7 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 
-export default function App() {
-
-  let test: number = 1
-  test = "test"
-
-  const testFunc = ():string => {
-    return "test"
-  }
-
+const App = ():JSX.Element => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!!!!</Text>
@@ -26,3 +18,5 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   }
 })
+
+export default App
