4c2a1627e35047f68edf2d032636a247b0d035bf
{UserTamura} <177226849+UserTamura@users.noreply.github.com>
2024-12-08 23:30:02 +0900

    Add additional interactions

--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -1,16 +1,19 @@
-import { View, Text, StyleSheet} from 'react-native'
+import {
+  View, Text, StyleSheet, TouchableOpacity
+} from 'react-native'
+import Icon from './Icon'
 
 const MemoListItem = ():JSX.Element => {
   return (
     <View style = { styles.memoListItem }>
-    <View>
-      <Text style = {styles.memoListItemTitle}>買い物リスト</Text>
-      <Text style = {styles.memoListItemDate}>2023年10月1日 10:00</Text>
+      <View>
+        <Text style = {styles.memoListItemTitle}>買い物リスト</Text>
+        <Text style = {styles.memoListItemDate}>2023年10月1日 10:00</Text>
+      </View>
+      <TouchableOpacity>
+        <Icon name='delete' size={32} color='#B0B0B0' />
+      </TouchableOpacity>
     </View>
-    <View>
-      <Text>X</Text>
-    </View>
-  </View>
   )
 }
 
