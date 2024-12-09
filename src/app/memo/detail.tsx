64ba8669fdbec4865e035ade45c97745a78ae714
{UserTamura} <177226849+UserTamura@users.noreply.github.com>
2024-12-09 22:22:55 +0900

    Customize header by removing header component

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -1,6 +1,5 @@
 import { View, Text, ScrollView, StyleSheet} from 'react-native'
 import { router } from 'expo-router'
-import Header from '../../components/Header'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 
@@ -11,7 +10,6 @@ const handlePress = ():void => {
 const Detail = ():JSX.Element =>{
   return (
     <View style= {styles.container}>
-      <Header />
       <View style = {styles.memoHeader}>
         <Text style ={styles.memoTitle}>買い物リスト</Text>
         <Text style={styles.memoDate}>2023年10月1日 10:00</Text>
@@ -22,7 +20,7 @@ const Detail = ():JSX.Element =>{
         書体やレイアウトなどを確認するために用います。本文用なので使い方を間違えると不自然に見えることもありますので要注意。
         </Text>
       </ScrollView>
-      <CircleButton onPress={handlePress} style={{ top: 160, bottom: 'auto'}}>
+      <CircleButton onPress={handlePress} style={{ top: 60, bottom: 'auto'}}>
         <Icon name='pencil' size={40} color='#FFF'/>
         </CircleButton>
     </View>
