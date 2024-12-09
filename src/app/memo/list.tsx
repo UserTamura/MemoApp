15fea55d0d3fe3bbed991eeaa797689f5a252838
{UserTamura} <177226849+UserTamura@users.noreply.github.com>
2024-12-10 08:14:11 +0900

    Implement full-screen navigation using useEffect

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,22 +1,25 @@
-import { View, Text, StyleSheet} from "react-native"
+import { View, StyleSheet} from "react-native"
 import MemoListItem from "../../components/MemoListItem"
 import CircleButton from "../../components/CircleButton"
 import Icon from '../../components/Icon'
 import { router, useNavigation } from "expo-router"
+import { useEffect } from "react"
+import LogOutButton from "../../components/LogOutButton"
 
 const handlePress = ():void => {
   router.push('/memo/create')
 }
 
+
 const List = ():JSX.Element => {
+  //headerの右側に文字
   const navigation = useNavigation()
-  navigation.setOptions({
-    headerRight: () => {
-      return (
-        <Text>Text</Text>
-      )
-    }
-  })
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => { return <LogOutButton /> }
+    })
+  },[])
+
   return (
     <View style = {styles.container}>
       <View>
