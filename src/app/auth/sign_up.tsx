f65425b51d39ff35557e383014a43fc74ade74fa
{UserTamura} <177226849+UserTamura@users.noreply.github.com>
2024-12-13 06:36:00 +0900

    Implement login functionality

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,13 +1,28 @@
 import {
-  View, Text, TextInput, StyleSheet, TouchableOpacity
+  View, Text, TextInput, StyleSheet, TouchableOpacity,
+  Alert
 } from 'react-native'
 import { Link, router } from 'expo-router'
 import { useState } from 'react'
 import Button from '../../components/Button'
+import { auth } from '../../config'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 
-const handlePress = (): void => {
+
+const handlePress = (email: string, password: string): void => {
   //会員登録
-  router.push('/memo/list')
+  console.log(email, password)
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    console.log(userCredential.user.uid)
+    //pushだと左上にバックボタン出てしまうのでreplace
+    router.replace('/memo/list')
+  })
+  .catch((error) => {
+    const {code, message} = error
+    console.log(code, message)
+    Alert.alert(message)
+  })
 }
 
 const SignUp = ():JSX.Element => {
@@ -21,10 +36,10 @@ const SignUp = ():JSX.Element => {
         <TextInput style={styles.input} value={email} onChangeText={(text) => {setEmail(text)}} autoCapitalize='none' keyboardType='email-address' placeholder='Email Address' textContentType='emailAddress' />
         <TextInput style={styles.input} value={password} onChangeText={(password
         ) => {setPassword(password)}} autoCapitalize='none' secureTextEntry placeholder='Password' textContentType='password' />
-        <Button label='Submit' onPress={handlePress} />
+        <Button label='Submit' onPress={() => { handlePress(email,password) }}  />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Already registered?</Text>
-          <Link href='/auth/log_in' asChild>
+          <Link href='/auth/log_in' asChild replace>
             <TouchableOpacity>
               <Text style={styles.footerLink}>Log in. </Text>
             </TouchableOpacity>
